6f8867c83dbca7ac4212b66a7e88be5bfb599238
Samuel Ogu <ogusam32@gmail.com>
2025-04-07 18:22:41 +0200

    first commit

--- src/components/personalBanking/PageThree.tsx
+++ src/components/personalBanking/PageThree.tsx
@@ -5,7 +5,7 @@ import { Label } from "@/components/ui/label";
 interface StepProps {
   formData: Record<string, any>;
   handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  handleCheckboxChange?: (checked: boolean) => void;
+  
   errors?: Record<string, string>;
 }
  const PageThree = ({ formData, handleChange, errors }: StepProps)=>
@@ -14,47 +14,47 @@ interface StepProps {
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
             <div>
         <Label htmlFor="signatoryName">Signatory Name</Label>
-        <Input id="signatoryName" name="signatoryName" placeholder="Full Name" value={formData.signatoryName || ""} onChange={handleChange} />
+        <Input id="signatoryName" name="signatoryName" placeholder="Full Name" value={formData.signatoryName || ""} onChange={handleChange} className="text-white mt-2"/>
         {errors?.signatoryName && <p className="text-red-500 text-sm">{errors.signatoryName}</p>}
             </div>
             <div>
               <Label htmlFor="bvn">BVN</Label>
-              <Input id="bvn" name="bvn" placeholder="BVN" value={formData.bvn || ""} onChange={handleChange} className="text-white"/>
+              <Input id="bvn" name="bvn" placeholder="BVN" value={formData.bvn || ""} onChange={handleChange} className="text-white mt-2"/>
               {errors?.bvn && <p className="text-red-500 text-sm">{errors.bvn}</p>}
             </div>
       
             <div>
               <Label htmlFor="placeOfBirth">Place of Birth</Label>
-              <Input id="placeOfBirth" name="placeOfBirth" placeholder="Place of Birth" value={formData.placeOfBirth || ""} onChange={handleChange} className="text-white"/>
+              <Input id="placeOfBirth" name="placeOfBirth" placeholder="Place of Birth" value={formData.placeOfBirth || ""} onChange={handleChange} className="text-white mt-2"/>
               {errors?.placeOfBirth && <p className="text-red-500 text-sm">{errors.placeOfBirth}</p>}
             </div>
       
             <div>
               <Label htmlFor="gender">Gender</Label>
-              <Input id="gender" name="gender" placeholder="Gender" value={formData.gender || ""} onChange={handleChange} className="text-white"/>
+              <Input id="gender" name="gender" placeholder="Gender" value={formData.gender || ""} onChange={handleChange} className="text-white mt-2"/>
               {errors?.gender && <p className="text-red-500 text-sm">{errors.gender}</p>}
             </div>
       
             <div>
               <Label htmlFor="signatoryAddress">Address</Label>
-              <Input id="signatoryAddress" name="signatoryAddress" placeholder="Address" value={formData.signatoryAddress || ""} onChange={handleChange} className="text-white"/>
+              <Input id="signatoryAddress" name="signatoryAddress" placeholder="Address" value={formData.signatoryAddress || ""} onChange={handleChange} className="text-white mt-2"/>
               {errors?.signatoryAddress && <p className="text-red-500 text-sm">{errors.signatoryAddress}</p>}
             </div>
       
             <div>
               <Label htmlFor="signatoryPhone">Phone Number</Label>
-              <Input id="signatoryPhone" name="signatoryPhone" placeholder="Phone Number" value={formData.signatoryPhone || ""} onChange={handleChange} className="text-white"/>
+              <Input id="signatoryPhone" name="signatoryPhone" placeholder="Phone Number" value={formData.signatoryPhone || ""} onChange={handleChange} className="text-white mt-2"/>
               {errors?.signatoryPhone && <p className="text-red-500 text-sm">{errors.signatoryPhone}</p>}
             </div>
             <div>
              <Label htmlFor="businessEmail"> Email</Label>
-            <Input id="signatoryEmail" name="signatoryEmail" placeholder=" Email" value={formData.signatoryEmail || ""} onChange={handleChange} className="text-white" />
+            <Input id="signatoryEmail" name="signatoryEmail" placeholder=" Email" value={formData.signatoryEmail || ""} onChange={handleChange} className="text-white mt-2" />
              {errors?.signatoryEmail && <p className="text-red-500 text-sm">{errors.signatoryEmail}</p>}
             </div>
 
             <div>
               <Label htmlFor="idDocument">ID Document</Label>
-              <Input id="idDocument" name="idDocument" type="file" onChange={handleChange} />
+              <Input id="idDocument" name="idDocument" type="file" onChange={handleChange}  className="mt-2"/>
               {errors?.idDocument && <p className="text-red-500 text-sm">{errors.idDocument}</p>}
             </div>
           </div>
--- src/components/personalBanking/PageTwo.tsx
+++ src/components/personalBanking/PageTwo.tsx
@@ -15,43 +15,43 @@ const PageTwo = ({ formData, handleChange, errors }: StepProps) => {
 <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
       <div>
         <Label htmlFor="businessName">Business Name</Label>
-        <Input id="businessName" name="businessName" placeholder="Business Name" value={formData.businessName || ""} onChange={handleChange} className="text-white"/>
+        <Input id="businessName" name="businessName" placeholder="Business Name" value={formData.businessName || ""} onChange={handleChange} className="text-white mt-2"/>
         {errors?.businessName && <p className="text-red-500 text-sm">{errors.businessName}</p>}
       </div>
 
       <div>
         <Label htmlFor="businessAddress">Business Address</Label>
-        <Input id="businessAddress" name="businessAddress" placeholder="Business Address" value={formData.businessAddress || ""} onChange={handleChange} className="text-white"/>
+        <Input id="businessAddress" name="businessAddress" placeholder="Business Address" value={formData.businessAddress || ""} onChange={handleChange} className="text-white mt-2"/>
         {errors?.businessAddress && <p className="text-red-500 text-sm">{errors.businessAddress}</p>}
       </div>
 
       <div>
         <Label htmlFor="phoneNumber">Phone Number</Label>
-        <Input id="phoneNumber" name="phoneNumber" placeholder="Phone Number" value={formData.phoneNumber || ""} onChange={handleChange} className="text-white"/>
+        <Input id="phoneNumber" name="phoneNumber" placeholder="Phone Number" value={formData.phoneNumber || ""} onChange={handleChange} className="text-white mt-2"/>
         {errors?.phoneNumber && <p className="text-red-500 text-sm">{errors.phoneNumber}</p>}
       </div>
 
       <div>
         <Label htmlFor="businessEmail">Business Email</Label>
-        <Input id="businessEmail" name="businessEmail" placeholder="Business Email" value={formData.businessEmail || ""} onChange={handleChange} className="text-white"/>
+        <Input id="businessEmail" name="businessEmail" placeholder="Business Email" value={formData.businessEmail || ""} onChange={handleChange} className="text-white mt-2"/>
         {errors?.businessEmail && <p className="text-red-500 text-sm">{errors.businessEmail}</p>}
       </div>
 
       <div>
         <Label htmlFor="cacNumber">CAC Number</Label>
-        <Input id="cacNumber" name="cacNumber" placeholder="CAC Number" value={formData.cacNumber || ""} onChange={handleChange} className="text-white"/>
+        <Input id="cacNumber" name="cacNumber" placeholder="CAC Number" value={formData.cacNumber || ""} onChange={handleChange} className="text-white mt-2"/>
         {errors?.cacNumber && <p className="text-red-500 text-sm">{errors.cacNumber}</p>}
       </div>
 
       <div>
         <Label htmlFor="lga">Local Government Area (L.G.A)</Label>
-        <Input id="lga" name="lga" placeholder="Local Government Area (L.G.A)" value={formData.lga || ""} onChange={handleChange} className="text-white" />
+        <Input id="lga" name="lga" placeholder="Local Government Area (L.G.A)" value={formData.lga || ""} onChange={handleChange} className="text-white mt-2" />
         {errors?.lga && <p className="text-red-500 text-sm">{errors.lga}</p>}
       </div>
 
       <div>
         <Label htmlFor="state">State</Label>
-        <Input id="state" name="state" placeholder="State" value={formData.state || ""} onChange={handleChange} className="text-white"/>
+        <Input id="state" name="state" placeholder="State" value={formData.state || ""} onChange={handleChange} className="text-white mt-2"/>
         {errors?.state && <p className="text-red-500 text-sm">{errors.state}</p>}
       </div>
     </div>
